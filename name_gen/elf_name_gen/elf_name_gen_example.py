a294fe960fb734cd5358be743b656331dcb30672
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-18 14:19:43 -0500

    Clean up a few PEP8 problems

--- name_gen/elf_name_gen/elf_name_gen_example.py
+++ name_gen/elf_name_gen/elf_name_gen_example.py
@@ -1,23 +1,23 @@
 from elf_name_gen import *
 
 for i in range(10):
-	print(random_elf_name())
+    print(random_elf_name())
 
 print
 
 for i in range(10):
-	name   = random_elf_name()
-	gender = elf_name_grammatical_gender(name)
+    name = random_elf_name()
+    gender = elf_name_grammatical_gender(name)
 
-	print("{}, {}".format(name, gender))
+    print("{}, {}".format(name, gender))
 
 print
 
 for i in range(10):
-	name   = random_elf_name()
-	gender = elf_name_grammatical_gender(name)
+    name = random_elf_name()
+    gender = elf_name_grammatical_gender(name)
 
-	if gender == 'neuter':
-		gender = resolve_gender_neutrality(name)
-		
-	print("{}, {}".format(name, gender))
+    if gender == 'neuter':
+        gender = resolve_gender_neutrality(name)
+
+    print("{}, {}".format(name, gender))
