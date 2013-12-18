fad7d1673e38e0d47d78de42740f16d2e9f99c0d
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-18 14:09:22 -0500

    Clean up a few PEP8 problems

--- city_generator/city_gen.py
+++ city_generator/city_gen.py
@@ -1,105 +1,107 @@
 import random
 
-city_sizes = {"village" : (25, 100), "town" : (101, 500),
-		"large town" : (501, 1000), "city" : (1001, 3000),
-		"large city" : (3001, 5000), "metrolpolis" : (5001, 10000)}
+city_sizes = {"village": (25, 100), "town": (101, 500),
+              "large town": (501, 1000), "city": (1001, 3000),
+              "large city": (3001, 5000), "metropolis": (5001, 10000)}
 
 with open("city_name_syllables.txt") as f:
-	city_name_syllables = f.read().splitlines()
+    city_name_syllables = f.read().splitlines()
 
 len_city_syllables = len(city_name_syllables)
 
+
 def random_city_size():
-	"""
-	Returns a tuple of type (string, int),
-	representing the type of city and it's 
-	population
-	"""
-	city_type = random.choice(city_sizes.keys())
+    """
+    Returns a tuple of type (string, int),
+    representing the type of city and it's
+    population
+    """
+    city_type = random.choice(city_sizes.keys())
 
-	lower_size_bound = city_sizes[city_type][0]
-	upper_size_bound = city_sizes[city_type][1]
+    lower_size_bound = city_sizes[city_type][0]
+    upper_size_bound = city_sizes[city_type][1]
 
-	city_size = random.randrange(lower_size_bound, upper_size_bound)
+    city_size = random.randrange(lower_size_bound, upper_size_bound)
 
-	return (city_type, city_size)
+    return city_type, city_size
+
 
 def simple_city_name():
-	"""
-	Returns a random city name, with component syllables drawn from
-	city_name_syllables.txt
-	"""
+    """
+    Returns a random city name, with component syllables drawn from
+    city_name_syllables.txt
+    """
 
-	return pattern_city_name("{U}{}")
+    return pattern_city_name("{U}{}")
 
 
 def city_name(syllables):
-	"""
-	n-syllable name city name
-	param: syllables should be type int
-	"""
+    """
+    n-syllable name city name
+    param: syllables should be type int
+    """
 
-	rand1 = random.randrange(len_city_syllables)
-	first = city_name_syllables[rand1]
-	last  = []
+    rand1 = random.randrange(len_city_syllables)
+    first = city_name_syllables[rand1]
+    last = []
 
-	for i in range(syllables):
-		n = random.randrange(len_city_syllables)
-		last.append(city_name_syllables[n].lower())
+    for i in range(syllables):
+        n = random.randrange(len_city_syllables)
+        last.append(city_name_syllables[n].lower())
 
-	last = "".join(last)
-	return first + last
+    last = "".join(last)
+    return first + last
+
 
 def random_city(syllables):
-	name       = city_name(syllables)
-	city_type  = random_city_size()
-	size       = city_type[0]
-	population = city_type[1]
+    name = city_name(syllables)
+    city_type = random_city_size()
+    size = city_type[0]
+    population = city_type[1]
 
-	return "{}, a {} with a population of {}".format(name, size, population)
+    return "{}, a {} with a population of {}".format(name, size, population)
 
 
-# Ornate city names (varoious stylizations)
 def ornate_city_name_1():
-	"""
-	Returns a city name, stylized as C'Cc, where C is a capitlized
-	syllable drawn from city_name_syllables and Cc is drawn from
-	simple_city_name()
-	"""
+    """
+    Returns a city name, stylized as C'Cc, where C is a capitalized
+    syllable drawn from city_name_syllables and Cc is drawn from
+    simple_city_name()
+    """
 
-	return pattern_city_name("{U}'{}")
+    return pattern_city_name("{U}'{}{}")
+
 
 def ornate_city_name_2():
+    return pattern_city_name("{U}-{}")
 
-	return pattern_city_name("{U}-{}")
 
 def pattern_city_name(pattern):
-	"""
-	Returns a city name, stylized with an arbitrary pattern, where
-	{U} gives a capital syllable and {} gives a lower case syllable.
-	"""
+    """
+    Returns a city name, stylized with an arbitrary pattern, where
+    {U} gives a capital syllable and {} gives a lower case syllable.
+    """
 
-	for symbol in ["{U}", "{}"]:
-		while symbol in pattern:
-			rand = random.randrange(len_city_syllables)
-			new_syllable = ""
-			if symbol is "{U}":
-				new_syllable = city_name_syllables[rand]
-			else:
-				new_syllable = city_name_syllables[rand].lower()
-			pattern = pattern.replace(symbol, new_syllable, 1)
-	return pattern
+    for symbol in ["{U}", "{}"]:
+        while symbol in pattern:
+            rand = random.randrange(len_city_syllables)
+            if symbol is "{U}":
+                new_syllable = city_name_syllables[rand]
+            else:
+                new_syllable = city_name_syllables[rand].lower()
+            pattern = pattern.replace(symbol, new_syllable, 1)
+    return pattern
 
 
 def simple_random_city():
-	"""
-	Returns a two-syllable, unstylized random city name
-	"""
+    """
+    Returns a two-syllable, non-stylized random city name
+    """
 
-	name       = simple_city_name()
-	city_type  = random_city_size()
+    name = simple_city_name()
+    city_type = random_city_size()
 
-	size       = city_type[0]
-	population = city_type[1]
+    size = city_type[0]
+    population = city_type[1]
 
-	return "{}, a {} with a population of {}".format(name, size, population)
+    return "{}, a {} with a population of {}".format(name, size, population)
