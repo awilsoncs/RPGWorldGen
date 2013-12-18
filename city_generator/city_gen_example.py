2ab5d0e494b36a31439bda6ec7350ee6f29dcceb
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-18 14:10:32 -0500

    Clean up a few PEP8 problems

--- city_generator/city_gen_example.py
+++ city_generator/city_gen_example.py
@@ -5,19 +5,19 @@ from city_gen import *
 # Examples by https://github.com/TheNewtInTheSuit
 
 for i in range(10):
-	print(simple_random_city())
+    print(simple_random_city())
 
 print
 
 for i in range(4):
-	print(random_city(i))
+    print(random_city(i))
 
 print
 
 for i in range(3):
-	print(ornate_city_name_1())
+    print(ornate_city_name_1())
 
 print
 
 for i in range(3):
-	print(ornate_city_name_2())
+    print(ornate_city_name_2())
