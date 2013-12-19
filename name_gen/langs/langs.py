262339e73b62597c8637bb5285da92f48775f499
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-19 01:53:46 -0500

    Put predefined langauges in the langs file

--- name_gen/langs/langs.py
+++ name_gen/langs/langs.py
@@ -145,3 +145,33 @@ def lang_from_file(syllables):
     Takes a text file of language syllables and returns a language based on it.
     """
     pass
+
+# Predefined languages
+dwarvish = Language(u"aouiôeá",
+                    "rbldmfhjngtkvs",
+                    "(C)(V)(V?)(C)",
+                    name_suffix="io",
+                    name_patterns=["{U} {U}{w}", "{U} of {U}{}", "{U} of House {U}", "{U}, son of {U}",
+                                   "{U}, daughter of {U}", "{U} of the {U}{}"])
+
+# Elves prefer longer words, some punctuation
+elvish = Language(u"iueaóëáéôîûú",
+                  "lncrshtbdz",
+                  "(C?)(V)(C)",
+                  word_length=2.7,
+                  punctuation="'",
+                  name_suffix=["i", "ia", "ue", "as"],
+                  punctuation_rarity=2,
+                  name_patterns=["{U} {U}{w}", "{U} of {U}{}", "{U} of House {U}", "{U}, son of {U}",
+                                 "{U}, daughter of {U}", "{U} of the {U}{}"])
+
+# Goblins like ugly doubles and have fewer syllables.
+# This language displays how to use sounds that take multiple letters in english.
+goblin = Language("ouae",
+                  ["b", "r", "g", "ch", "m",
+                   "t", "z", "l", "k", "h", "s"],
+                  "(C)(V)(V?)(C)",
+                  word_length=1,
+                  clean_doubles=False,
+                  phoneme_count=20,
+                  name_patterns=["{U}"])
--- name_gen/langs/langs_examples.py
+++ name_gen/langs/langs_examples.py
@@ -1,49 +1,21 @@
 # coding=utf-8
 from name_gen.langs.langs import *
 
-dwarvish = Language(u"aouiôeá",
-                    "rbldmfhjngtkvs",
-                    "(C)(V)(V?)(C)",
-                    name_suffix="io",
-                    name_patterns=["{U} {U}{w}", "{U} of {U}{}", "{U} of House {U}", "{U}, son of {U}",
-                                   "{U}, daughter of {U}", "{U} of the {U}{}"])
-
-# Elves prefer longer words, some punctuation
-elvish = Language(u"iueaóëáéôîûú",
-                  "lncrshtbdz",
-                  "(C?)(V)(C)",
-                  word_length=3,
-                  punctuation="'",
-                  name_suffix=["i", "ia", "ue", "as"],
-                  punctuation_rarity=2,
-                  name_patterns=["{U} {U}{w}", "{U} of {U}{}", "{U} of House {U}", "{U}, son of {U}",
-                                 "{U}, daughter of {U}", "{U} of the {U}{}"])
-
-# Goblins like ugly doubles and have fewer syllables.
-# This language displays how to use sounds that take multiple letters in english.
-goblin = Language("ouae",
-                  ["b", "r", "g", "ch", "m",
-                   "t", "z", "l", "k", "h", "s"],
-                  "(C)(V)(V?)(C)",
-                  clean_doubles=False,
-                  phoneme_count=20,
-                  name_patterns=["{U}"])
-
 print "\nDwarvish Names\n"
 for _ in range(5):
     print dwarvish.pattern_name()
-print "\nDwarvish Phrases\n"
+print "\nDwarvish Samples\n"
 for _ in range(5):
-    print dwarvish.phrase()
+    print dwarvish.sample()
 print "\nElvish Names\n"
 for _ in range(5):
     print elvish.pattern_name()
-print "\nElvish Phrases\n"
+print "\nElvish Samples\n"
 for _ in range(5):
-    print elvish.phrase()
+    print elvish.sample()
 print "\nGoblin Names\n"
 for _ in range(5):
     print goblin.pattern_name()
-print "\nGoblin Phrases\n"
+print "\nGoblin Samples\n"
 for _ in range(5):
-    print goblin.phrase()
+    print goblin.sample()
