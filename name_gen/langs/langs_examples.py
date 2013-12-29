9936353c728142d483e476219d14b0311ca7b064
Aaron Wilson <aaronwilsonguitar@gmail.com>
2013-12-29 14:52:05 -0500

    Move langs to its own file

--- name_gen/langs/langs_examples.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from name_gen.langs.langs import *
-
-print "\nElvish Names\n"
-for _ in range(3):
-    print elvish.pattern_name()
-print "\nElvish Words\n"
-for _ in range(3):
-    print elvish.sample()
